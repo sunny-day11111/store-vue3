db8a340a57610cab6e1a863b2478407a0caac0ed
Ruiqi Wang <ruiqi.wang@nx-engine.com>
2022-07-13 17:20:29 +0800

    aaaa

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path' // 如果报错就安装 npm i -D @types/node
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+})
